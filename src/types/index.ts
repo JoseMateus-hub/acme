115f2a2c060afe6ebfb292473d47668b665f88a0
José Mateus | Desenvolvedor Full Stack <josemateus957@gmail.com>
2026-03-31 14:54:51 +0000

    feat: implementar crud de clientes na camada de servico

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,7 +12,7 @@ export interface Customer {
   id: string;
   name: string;
   email: string;
-  imaageUrl: string;
+  imageUrl: string;
 };
 
 export type CreateCustomerData = Omit<Customer, 'id'>;
